bc1c73b941a7fc622730857195a88dd8dbdd0cff
kerosinn <trimp@yandex.ru>
2021-05-18 14:46:01 +0300

    added checksum subtract data and tested

--- app/src/main/java/com/hifitoy/hifitoynumbers/Checksummer.java
+++ app/src/main/java/com/hifitoy/hifitoynumbers/Checksummer.java
@@ -6,6 +6,9 @@
  */
 package com.hifitoy.hifitoynumbers;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class Checksummer {
     public static short calc(byte[] data) {
         byte sum = 0;
@@ -18,6 +21,23 @@ public class Checksummer {
 
         short checkSum = (short)(sum & 0xFF);
         checkSum |= (short)((fibonacci << 8) & 0xFF00);
+        return checkSum;
+    }
+
+    //we have checksum(CS) value of data with originalLength
+    //func recalculate CS subtracting first data[]
+    public static short subtractData(short checksum, int originalLength, byte[] data) {
+        ByteBuffer b = ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort(0, checksum);
+        byte sum = b.get();
+        byte fib = b.get();
+
+        for (int i = 0; i < data.length; i++) {
+            sum -= data[i];
+            fib -= data[i] * (originalLength - i);
+        }
+
+        short checkSum = (short)(sum & 0xFF);
+        checkSum |= (short)((fib << 8) & 0xFF00);
         return checkSum;
     }
 }
