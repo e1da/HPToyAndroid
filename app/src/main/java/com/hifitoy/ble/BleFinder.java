b4b539ba7053344b519f9dc7838f84aad1284bb1
Artem Khlyupin <trimp@yandex.ru>
2026-04-20 03:23:39 +0400

    BleFinder: adds permission checker to addDeviceToList()

--- app/src/main/java/com/hifitoy/ble/BleFinder.java
+++ app/src/main/java/com/hifitoy/ble/BleFinder.java
@@ -7,7 +7,6 @@
 
 package com.hifitoy.ble;
 
-import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.ScanCallback;
@@ -106,18 +105,23 @@ public class BleFinder {
         Log.d(TAG, "BLE Stop Scanning");
     }
 
-    @SuppressLint("MissingPermission")
-    private void addDeviceToList(BluetoothDevice bd) {
-        String peripheralName = bd.getName();
+    private void addDeviceToList(BluetoothDevice device) {
+        if (!permissionService.hasBluetoothConnectPermission()) return;
 
-        if ((peripheralName != null) && (!deviceAddressList.contains(bd.getAddress()))) {
-            deviceAddressList.add(bd.getAddress());
+        try {
+            String name = device.getName();
+            String address = device.getAddress();
 
-            Log.d(TAG, "Find ble device: " + peripheralName + " " + bd.getAddress());
+            if ((name != null) && (!deviceAddressList.contains(address))) {
+                deviceAddressList.add(address);
+                Log.d(TAG, "Find ble device: " + name + " " + address);
 
-            if (delegate != null) {
-                delegate.didFindNewPeripheral(bd.getAddress(), peripheralName);
+                if (delegate != null) {
+                    delegate.didFindNewPeripheral(address, name);
+                }
             }
+        } catch (SecurityException e) {
+            Log.w(TAG, "BLE device info permission denied.");
         }
     }
 
